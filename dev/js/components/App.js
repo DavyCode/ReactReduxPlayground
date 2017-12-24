4969c408141ea893a2c974ea71087519357f15d8
Davy'd <pdaswift3009@gmail.com>
2017-12-24 16:50:28 +0100

    consume action in reducer

--- dev/js/components/App.js
+++ dev/js/components/App.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
-import Booklist from '../containers/book-list'
+import BookList from '../containers/book-list'
+import BookDetail from '../containers/book-detail';
 require('../../scss/style.scss');
 
 
 export default class App extends Component {
     render() {
         return ( 
-            <div>
-                <h1> welcome to the app</h1>
-                <Booklist />
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-lg-10'>
+                        <h1> Welcome select a book</h1>
+                    <hr/>
+                    </div>
+                    <BookList />
+                    <BookDetail />
+                </div>
             </div>
         );
     }
--- dev/js/reducers/reducer_books.js
+++ dev/js/reducers/reducer_books.js
@@ -1,10 +1,10 @@
 export default function() {
     return [
-        {title: 'It is christmass'},
-        {title: 'Chrismass trilogy'},
-        {title: 'Chicken\'s population under threat  '},
-        {title: 'Gifts galour'},
-        {title: 'Christmass movie'},
-        {title: 'Party everywhere'},
+        {title: 'It is christmass',  pages :34},
+        {title: 'Chrismass trilogy', pages :333},
+        {title: 'Chicken\'s population under threat',  pages :4},
+        {title: 'Gifts galour',  pages :3},
+        {title: 'Christmass movie',  pages :234},
+        {title: 'Party everywhere',  pages :391},
     ]
 }
